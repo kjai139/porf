2ea83307246f54cc2903ccb2cc45942cd5cbd7c1
kj139 <therealkjai139@gmail.com>
2024-08-09 22:42:16 -0400

    start particle bg

---
+++ src/app/_components/backgrounds/ParticlesBg.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { useEffect, useRef } from "react"
+
+export default function ParticleBg () {
+    const canvasRef = useRef(null)
+    
+    
+
+    class Particle {
+        x: number
+        y:number
+        size: number
+        speedX:number
+        speedY:number
+        
+        constructor(x:number, y:number) {
+            this.x = x;
+            this.y = y;
+            this.size = Math.random() * 5 + 1;
+            this.speedX = Math.random() * 3 - 1.5;
+            this.speedY = Math.random() * 3 - 1.5;
+            
+        }
+
+        update() {
+            this.x += this.speedX
+            this.y += this.speedY
+            if (this.size > 0.2) this.size -= 0.1;
+            
+        }
+
+        draw(ctx) {
+            ctx.beginPath()
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+            ctx.fillStyle = `rgba(255, 255, 255)`
+            ctx.closePath()
+            ctx.fill()
+
+        }
+    }
+
+    
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const ctx= canvas.getContext('2d')
+        let particlesArray = []
+        const numberOfParticles = 100
+        const canvasWidth = canvas.width = window.innerWidth;
+        const canvasHeight = canvas.height = window.innerHeight;
+        const resizeCanvas = () => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+          };
+      
+        const createParticles = () => {
+            for (let i = 0; i < numberOfParticles; i++) {
+              const x = Math.random() * canvasWidth;
+              const y = Math.random() * canvasHeight;
+              
+      
+              particlesArray.push(new Particle(x, y));
+            }
+          };
+      
+          const animateParticles = () => {
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      
+            particlesArray.forEach((particle) => {
+              particle.update();
+              particle.draw(ctx);
+            });
+      
+            requestAnimationFrame(animateParticles);
+          };
+
+          const handleResize = () => {
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+          }
+          
+          window.addEventListener('resize', handleResize);
+          createParticles();
+          animateParticles();
+      
+          return () => {
+            window.removeEventListener('resize', resizeCanvas);
+          };
+    }, [])
+
+    return (
+        <canvas ref={canvasRef}>
+
+        </canvas>
+    )
+}
