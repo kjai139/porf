fc89593b391e44248023b3a40d890f2870e0066e
kj139 <therealkjai139@gmail.com>
2024-08-26 23:07:39 -0400

    dsadsd

--- src/components/nav/topNav.tsx
+++ src/components/nav/topNav.tsx
@@ -56,7 +56,7 @@ export default function TopNav () {
     
 
     return (
-        <nav className="flex w-full justify-between p-6 shadow text-foreground bg-navBg z-50 top-0 sticky">
+        <nav className="flex w-full justify-between p-4 shadow text-foreground bg-navBg z-50 top-0 sticky">
             <div>
                 {
                     !isDesktop ?
