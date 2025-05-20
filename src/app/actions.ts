c21dab9b5c0926db44fd19cc98ebe03f8f775445
kj139 <therealkjai139@gmail.com>
2025-05-20 00:12:45 -0400

    update oauth gmail

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,6 +4,9 @@ import { ContactFormType } from "@/components/forms/mailerForm"
 import { Redis } from "@upstash/redis"
 import { Ratelimit } from "@upstash/ratelimit"
 import { headers } from "next/headers"
+import { google } from 'googleapis'
+
+
 const redis = new Redis({
     url: process.env.REDIS_URL,
     token: process.env.REDIS_TOKEN
@@ -11,14 +14,27 @@ const redis = new Redis({
 
 const ratelimit = new Ratelimit({
     redis: redis,
-    limiter:Ratelimit.slidingWindow(1, '60s')
+    limiter: Ratelimit.slidingWindow(1, '60s')
 })
 
-export async function sendEmail(values:ContactFormType) {
-    
+const CLIENT_ID = process.env.GMAIL_CLIENT_ID
+const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN
+const USER_EMAIL = process.env.nodeGmailEmail
 
-        try {
-            console.log('Sa values -', values)
+const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    'https://developers.google.com/oauthplayground'
+)
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+
+export async function sendEmail(values: ContactFormType) {
+
+
+    try {
+        console.log('Sa values -', values)
 
         const ip = headers().get('x-forwarded-for')
         if (!ip) {
@@ -30,37 +46,42 @@ export async function sendEmail(values:ContactFormType) {
         } else {
             console.log('IP from SA - ', ip)
             console.log('REMAINING:', remaining, 'LIMIT', limit)
-            
 
+            const { token } = await oAuth2Client.getAccessToken()
+            console.log('[SendEmail] Token received:', token)
             const transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 587,
-                secure: false,
+                service:'gmail',
                 auth: {
-                    user: process.env.nodeGmailEmail,
-                    pass: process.env.nodeGmailPw
+                    type: 'OAuth2',
+                    user: USER_EMAIL,
+                    clientId: CLIENT_ID,
+                    clientSecret: CLIENT_SECRET,
+                    refreshToken: REFRESH_TOKEN,
+                    accessToken: token
+
                 },
             })
 
 
             const mailOptions = {
-                from: process.env.nodeGmailEmail,
-                to: process.env.nodeGmailEmail,
+                from: USER_EMAIL,
+                to: USER_EMAIL,
                 subject: `New message from portf website - from ${values.senderName}, ${values.senderEmail}`,
                 text: values.senderMessage,
             }
-                const info = await transporter.sendMail(mailOptions)
-                console.log('Email sent: ', info)
-                return 'success'
-        }} catch (err) {
-            console.log(err)
-            throw new Error('A server error has occured')
+            const info = await transporter.sendMail(mailOptions)
+            console.log('Email sent: ', info)
+            return 'success'
         }
-    
-    
+    } catch (err) {
+        console.error('[SendMail] Error:', err)
+        throw new Error('A server error has occured')
+    }
 
 
-    
 
-    
+
+
+
+
 }
